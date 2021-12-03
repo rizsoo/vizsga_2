451ad86efc675b27827fd834ee952b88abcf8ec1
rizsoo <89772889+rizsoo@users.noreply.github.com>
2021-12-03 14:43:02 +0100

    eee

---
+++ myapp/src/Error.js
@@ -0,0 +1,9 @@
+function Error ({key}) {
+    return(
+        <div id="error">
+            <h3>ERROR</h3>
+        </div>
+    )
+}
+
+export default Error;
---
+++ myapp/src/data.js
@@ -0,0 +1,9 @@
+let data = [
+    {title: "Some fancy product", description: "Soo good asd", price: 70, review: "Good", reviewer: "John", discount: false, recommended: true},
+    {title: "Not so fancy product", description: "Not so good", price: 7, review: "Not good",reviewer: "George", discount: false, recommended: true},
+    {title: "A product", description: "It exists", price: 17, review: "Nice", reviewer: "Anna", discount: true, recommended: false},
+    {title: "Another fancy product", description: "Also good", price: 80, review: "Fancy", reviewer: "Zac", discount: false, recommended: false},
+    {title: "Who knows what", description: "You need it", price: 27, review: "Crappy", reviewer: "Rick", discount: true, recommended: false}
+  ]
+
+export default data;
